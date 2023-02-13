9d21846ea99044bfcb148825dec2ab2cdf945d46
rmunguia16 <rmunguia16@comunidad.unam.mx>
2023-02-13 01:32:26 -0600

    Carrito Vacio

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import CartItem from './CartItem'
+import React from "react";
+import CartItem from "./CartItem";
+import { useState } from "react";
+import styles from "./Cart.module.css";
+import sonic from "../../assets/icons/sonic.png";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
   return (
     <div>
-        {cart.map((item) => {return (<CartItem item={item}/>)})}
+      {cart == null ? (
+        cart.map((item) => {
+          return <CartItem item={item} />;
+        })
+      ) : (
+        <div className={styles.emptyCart}>
+          <h1 className={styles.title}>Tu carrito esta vacio</h1>
+          <div className={styles.dontWorry}>
+            <img src={sonic} className={styles.img} alt="dont worry" />
+            <h2 className={styles.text}>¡No te preocupes!<br/><br/>Vuelve al inicio y... <br/> !Seguro encuentras algo que te guste! <br/><br/>
+            </h2>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import cart from '../../assets/icons/car.png';
-import styles from './CartWidget.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import cart from "../../assets/icons/car.png";
+import styles from "./CartWidget.module.css";
 
-const CartWidget = ({carrito}) => {
+const CartWidget = ({ carrito }) => {
   return (
-    <div className={styles.container}>
-        <h2 className={styles.number}>{carrito == null ? '0' : carrito} </h2>
-        <img src={cart} className={styles.cart} alt='carrito'/>
-    </div>
-  )
-}
+    <Link to="/Carrito">
+      <div className={styles.container}>
+        <h2 className={styles.number}>{carrito == null ? "0" : carrito} </h2>
+        <img src={cart} className={styles.cart} alt="carrito" />
+      </div>
+    </Link>
+  );
+};
 
-export default CartWidget
+export default CartWidget;
